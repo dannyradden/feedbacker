0952d5c54bb7f06522534a3867ffafd72b220b7f
dannyradden <dradden@gmail.com>
2017-10-25 02:45:19 -0600

    Finish login/logout redirection Seperate Landing into new file

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Welcome to Feedbacker!</h1>
+      <h5>Collect feedback from your users</h5>
+    </div>
+  );
+};
+
+export default Landing;
