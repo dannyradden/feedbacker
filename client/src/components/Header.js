a362ab0321754b1ef0a999d04a6d36c8efe55ace
dannyradden <dradden@gmail.com>
2017-10-26 03:43:47 -0600

    Add credits to Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,7 +19,10 @@ class Header extends Component {
           <li key="1">
             <Payments />
           </li>,
-          <li key="2">
+          <li key="2" style={{ margin: '0px 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="3">
             <a href="/api/logout">Logout</a>
           </li>
         ];
@@ -29,7 +32,11 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper light-blue">
-          <Link to={this.props.auth ? '/surveys' : '/'} className="brand-logo">
+          <Link
+            style={{ 'margin-left': '10px' }}
+            to={this.props.auth ? '/surveys' : '/'}
+            className="brand-logo"
+          >
             Feedbacker
           </Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
