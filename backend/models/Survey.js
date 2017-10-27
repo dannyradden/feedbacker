dd28d2f4747d1421309a0b24958cbc5f1ed0ce28
dannyradden <dradden@gmail.com>
2017-10-27 00:33:31 -0600

    Start building 'api/surveys' POST endpoint Add sendgrid key to dev and prod

--- backend/models/Survey.js
+++ backend/models/Survey.js
@@ -2,21 +2,23 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const RecipientSchema = require('./Recipients');
 
-const surveySchema = new Schema({
-  title: String,
-  body: String,
-  subject: String,
-  recipients: [RecipientSchema],
-  yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 },
-  _owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const surveySchema = new Schema(
+  {
+    title: String,
+    body: String,
+    subject: String,
+    recipients: [RecipientSchema],
+    yes: { type: Number, default: 0 },
+    no: { type: Number, default: 0 },
+    _owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    dateSent: Date,
+    lastResponded: Date
   },
-  dateSent: Date,
-  lastResponded: Date,
-  timestamps: true
-});
+  { timestamps: true }
+);
 
 mongoose.model('Survey', surveySchema);
