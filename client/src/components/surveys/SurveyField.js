9e835675fc92f4340c58af9eda4566f069027828
dannyradden <dradden@gmail.com>
2017-10-29 01:44:45 -0600

    Implement error checking on survey form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const SurveyField = ({ input, label }) => {
+const SurveyField = ({ input, label, type, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
+      <input type={type} data-error="wrong" style={{ marginBottom: '5px' }} {...input} />
+      {touched &&
+        error && (
+          <div className="red-text" style={{ marginBottom: '20px' }}>
+            <i className="material-icons left" style={{ marginRight: '4px' }}>
+              error
+            </i>
+            {error}
+          </div>
+        )}
     </div>
   );
 };
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
@@ -40,6 +41,28 @@ class SurveyForm extends Component {
   }
 }
 
+const validate = values => {
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
+
+  if (!values.title) {
+    errors.title = 'You must provide a title';
+  }
+  if (!values.subject) {
+    errors.subject = 'You must provide a subject';
+  }
+  if (!values.body) {
+    errors.body = 'You must provide a body';
+  }
+  if (!values.emails) {
+    errors.emails = 'You must provide at least one recipient';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
-  form: 'surveyForm'
+  form: 'surveyForm',
+  validate
 })(SurveyForm);
