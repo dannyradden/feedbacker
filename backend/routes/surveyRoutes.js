7a735c97f8a695d14eca18f7606666258dfd83f5
dannyradden <dradden@gmail.com>
2017-10-30 02:32:30 -0600

    Build parsing of survey webhook data in survey route.

--- backend/routes/surveyRoutes.js
+++ backend/routes/surveyRoutes.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Survey = mongoose.model('Survey');
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
@@ -11,7 +14,21 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
+
+    console.log(events);
+
     res.send({});
   });
 
@@ -27,7 +44,7 @@ module.exports = app => {
     });
 
     // Great place to send an email!
-    const mailer = new Mailer(survey, surveyTemplate(survey.body));
+    const mailer = new Mailer(survey, surveyTemplate(survey));
 
     try {
       await mailer.send();
